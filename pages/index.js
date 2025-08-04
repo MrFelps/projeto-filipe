afb9197de6822d3586f7cdf8a3e8257704b9dfe8
Felipe Gabriel B. de B. de Jesus <felipegabrielboccardo729@gmail.com>
2025-08-04 13:38:44 -0300

    modificação de texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Te amo, meu amor!!!</h1>
+    return <h1>Te amo, meu amor!!!
+        Amor da minha vida. Vontade de te morder.
+    </h1>
 }
 
 export default Home;
